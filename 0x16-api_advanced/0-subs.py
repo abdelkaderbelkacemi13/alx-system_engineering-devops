db2b0d5ed8c30c116994466422f2ca4ebf363d07
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-13 14:47:08 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This function queries the Reddit API 
+"""This function queries the Reddit API
     and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    number_of_subscribers returns the total number of subscribers 
+    number_of_subscribers returns the total number of subscribers
     """
     # Construct the URL for the Reddit API
     given_apiurl = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -15,7 +15,8 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     # Make a GET request to the Reddit API
-    http_response = requests.get(given_apiurl, headers=headers, allow_redirects=False)
+    http_response = requests.get(
+            given_apiurl, headers=headers, allow_redirects=False)
     # Check if the subreddit exist or not.
     if http_response.status_code == 404:
         return 0
