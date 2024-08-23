d4fc1c6bfe690c0da93ed29672a20f97f93c82f8
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-22 17:55:23 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,12 @@
 #update file descriptor limits
-
 exec { 'first-edit':
   provider => shell,
-  'sed -i -e "s/nofile 5/nofile 99999/" \
-	    -e "s/nofile 4/nofile 99999/" \
-	    /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 5/nofile 99999/" /etc/security/limits.conf',
+}
+
+
+exec { 'second-edit':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 99999/" /etc/security/limits.conf',
+  require  => Exec ['first-edit'],
 }
