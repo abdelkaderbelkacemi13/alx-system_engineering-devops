7cccdc6d920b0840aaea9d5ae6b37b195ab1a857
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-22 17:52:36 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,18 +1,8 @@
-#maximum number of file descriptors Nginx can open
+#update file descriptor limits
 
-#'fix-nginx' modifies the Nginx configuration file
-
-exec { 'fix-nginx':
+exec { 'first-edit':
   provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  path     => '/usr/local/bin/:/usr/bin'
-  notify   => Service['nginx']
-}
-
-#'nginx-restart' restarts the Nginx service
-
-exec { 'nginx-restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-  require  => Exec['fix-nginx'],
+  'sed -i -e "s/nofile 5/nofile 99999/" \
+	    -e "s/nofile 4/nofile 99999/" \
+	    /etc/security/limits.conf',
 }
