477e8f329658b70a15f8bb3dc3e8de00cabe45c4
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-22 18:05:00 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,11 @@
 #update file descriptor limits
-exec { 'first-edit':
-  provider => shell,
-  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  path     => '/usr/local/bin/:/bin/'
+exec {'first-edit':
+  command => 'sudo sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 
-exec { 'second-edit':
-  provider => shell,
-  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
-  path     => '/usr/local/bin/:/bin/'
-  require  => Exec ['first-edit'],
+exec {'second-edit':
+  command => 'sudo sed -i "/holberton soft/s/4/40000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
